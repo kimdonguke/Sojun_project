e49712ef191745e02c06cccc3e9bb6271e010441
kimdonguke <rlaehddnr100@gmail.com>
2019-12-03 09:03:30 +0900

    middle jot dam

--- app/src/main/java/com/example/sojun_project/activities/MakePrototype.java
+++ app/src/main/java/com/example/sojun_project/activities/MakePrototype.java
@@ -3,13 +3,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.sojun_project.Datas.ItemData;
 import com.example.sojun_project.R;
 import com.example.sojun_project.fragments.SimpleTextAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -18,11 +26,15 @@ public class MakePrototype extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<ItemData> morning, lunch, dinner;
     ArrayList<ItemData> itemData = new ArrayList<>();
-    Button save, next;
+    Button save, next, add;
+    SharedPreferences sharedPreferences;
+    TextView nowTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_prototype);
+
+        sharedPreferences= getSharedPreferences("sFile",MODE_PRIVATE);
 
         morning=new ArrayList<>();
         lunch=new ArrayList<>();
@@ -35,11 +47,27 @@ public class MakePrototype extends AppCompatActivity {
 
         save=findViewById(R.id.makeprototype_savebtn);
         next=findViewById(R.id.makeprototype_gonextbtn);
+        add=findViewById(R.id.makeprototype_addbtn);
+        nowTime=findViewById(R.id.makeprototype_timetext);
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                switch (nowTime.getText().toString()){
+                    case "MORNING":
+                        setStringArrayPref(MakePrototype.this,nowTime.getText().toString(),morning);
+                        break;
+                    case "LUNCH":
+                        setStringArrayPref(MakePrototype.this,nowTime.getText().toString(),morning);
+                        break;
+                    case "DINNER":
+                        setStringArrayPref(MakePrototype.this,nowTime.getText().toString(),morning);
+                        break;
+                        default:
+                            Log.e("make_prototype","timetext is null");
+                            break;
 
+                }
             }
         });
         next.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +76,43 @@ public class MakePrototype extends AppCompatActivity {
 
             }
         });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private void setStringArrayPref(Context context, String key, ArrayList<ItemData> values) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        editor.apply();
+    }
+
+    private ArrayList<String> getStringArrayPref(Context context, String key) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList<String> urls = new ArrayList<String>();
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
     }
 }
